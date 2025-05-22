53c405ac455154634a41f78e2d583c57312d112e
trngnneee <dtn06052005@gmail.com>
2025-05-22 17:09:22 +0700

    Thêm nút list mở Menu trên Mobile

--- client/src/app/components/Home/Header/Header.jsx
+++ client/src/app/components/Home/Header/Header.jsx
@@ -2,10 +2,11 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { CiCircleList } from "react-icons/ci";
+import { HiViewList } from "react-icons/hi";
 
 export const Header = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [open, setOpen] = useState(false)
 
   useEffect(() => {
     const handleRolled = () => {
@@ -15,17 +16,30 @@ export const Header = () => {
     return () => window.removeEventListener("scroll", handleRolled);
   })
 
-  const handleOpenMenu = () => {
-    console.log("ok");
-  }
-  
   return (
     <>
-      <div className={`flex justify-between items-center px-[30px] py-[20px] lg:py-[30px] bg-white sticky top-0 transition-shadow duration-300 z-50 rounded-b-[20px] ${
-        scrolled ? "shadow-xl" : ""
-      }`}>
-        <div className="block sm:hidden cursor-pointer" onClick={handleOpenMenu}>
-          <CiCircleList className="text-[15px]" />
+      {open && (
+        <div className="w-[120px] h-dvh bg-[#e0f2f5] fixed z-100 py-[20px] px-[5px]">
+          <div className="flex flex-col gap-[5px] border-b-[1px] border-b-[#cecbcb] pb-[20px]">
+            <Link href="#" className="text-[10px] font-[600] text-[#505050] hover:bg-[#5e5e5e33] px-[10px] py-[3px] rounded-[8px]">Product</Link>
+            <Link href="#" className="text-[10px] font-[600] text-[#505050] hover:bg-[#5e5e5e33] px-[10px] py-[3px] rounded-[8px]">Team</Link>
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      )}
+      {open && (
+        <div
+          className="bg-[#00000053] w-full h-full fixed z-80 cursor-pointer"
+          onClick={() => setOpen(false)}
+        ></div>
+      )}
+
+      <div className={`flex justify-between items-center px-[30px] py-[20px] lg:py-[30px] bg-white sticky top-0 transition-shadow duration-300 z-50 rounded-b-[20px] ${scrolled ? "shadow-xl" : ""
+        }`}>
+        <div className="block sm:hidden cursor-pointer" onClick={() => { setOpen(true) }}>
+          <HiViewList className="text-[15px]" />
         </div>
         <div className="flex items-center gap-[20px]">
           <Link href="/" className="w-[80px] sm:w-[100px] lg:w-[120px] h-auto">
